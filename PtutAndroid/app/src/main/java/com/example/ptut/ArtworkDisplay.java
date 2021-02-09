5cf14aae0ed745af6d93f77c9bfdbd6d1c48219d
haby <haby.romaric@gmail.com>
2021-02-09 21:29:42 +0100

    Android check permission

---
+++ PtutAndroid/app/src/main/java/com/example/ptut/ArtworkDisplay.java
@@ -0,0 +1,19 @@
+package com.example.ptut;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ArtworkDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_artwork_display);
+
+        TextView title = findViewById(R.id.tiltle);
+
+
+    }
+}
