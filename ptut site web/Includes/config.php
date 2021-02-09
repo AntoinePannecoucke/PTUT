cad4932514a33a1ec1a8c60cd62a3944e22f68aa
AntoinePannecoucke <60757025+AntoinePannecoucke@users.noreply.github.com>
2021-02-09 19:57:33 +0100

    Update config.php

--- ptut site web/Includes/config.php
+++ ptut site web/Includes/config.php
@@ -1,3 +1,3 @@
 <?php session_start();
-include "passwd.php";
+include "/passwd.php";
 $dbh = new PDO($dsn,$user,$password,[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
