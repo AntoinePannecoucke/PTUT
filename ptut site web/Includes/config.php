6aa9662a00f407795dd6c11ca7d89c8bcde0bcfe
AntoinePannecoucke <60757025+AntoinePannecoucke@users.noreply.github.com>
2021-02-09 19:32:49 +0100

    Update config.php

--- ptut site web/Includes/config.php
+++ ptut site web/Includes/config.php
@@ -1,9 +1,3 @@
 <?php session_start();
-$dsn = 'mysql:dbname=ptut;host=151.80.36.218';
-$user = 'beacon';
-$password = 'LR;%tbSY7x';
+include "passwd.php";
 $dbh = new PDO($dsn,$user,$password,[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-
-	/*$dsn = 'mysql:dbname=ptut;host=151.80.36.218';
-    $user = 'beacon';
-    $password = 'LR;%tbSY7x';*/
